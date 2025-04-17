7d080b7559de09e4d49a6776d08c71ebd37182ab
Ziyad Mabrouk <ziyadmabrouk@outlook.com>
2025-04-17 15:57:54 +0200

    fixed start.sh

--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Start the Python exporter in the background
-python3 /opt/oai-gnb/gnb_metrics_exporter.py &
+# Start the Python exporter in the background and log output
+python3 /opt/oai-gnb/gnb_metrics_exporter.py > /var/log/gnb_metrics_exporter.log 2>&1 &
 
 # Start the nr-softmodem process
 exec /opt/oai-gnb/bin/nr-softmodem "$@"
